9f25eb1fcb1bb11be2b049a61fe4263aef8c9602
AstaNovaldi <94747593+AstaNovaldi@users.noreply.github.com>
2024-11-19 13:53:38 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php 
+$nama = "Ripki Novaldi";
+$ttl = "Bandung 29 November 2007";
+$umur = 5 + 12;
+$alamat = "Bumi";
+
+echo "Nama: {$nama} <br> TTL: {$ttl} <br> Umur: {$umur} <br> Alamat: {$alamat}";
